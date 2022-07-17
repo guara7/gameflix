f1997100d176a177a5dfd417d41a71c363942a24
guara7 <38889742+guara7@users.noreply.github.com>
2022-07-17 12:46:40 -0300

    listagem de jogos e criação de uma rota para adicionar novos jogos

--- gameflix.py
+++ gameflix.py
@@ -1,9 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
+class Game:
+    def __init__(self,nome,categoria,console):
+        self.nome=nome
+        self.categoria=categoria
+        self.console=console
+
+listaJogos = [Game('Mario', 'Plataforma', 'Nintendo'),
+                  Game('CS GO', 'Tiro', 'PC'),
+                  Game('Last Of Us', 'Ação', 'PlayStation')]
 
 app = Flask(__name__)
 
-@app.route('/home')
-def ola():
-    return '<h2>ola mundo</h2>'
+@app.route('/')
+def home():
+    return render_template('lista.html', titulo='Games',jogos=listaJogos)
 
-app.run()
+@app.route('/novo')
+def novo1():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/criar', methods=['POST',])
+def criar1():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    novoJogo = Game(nome,categoria,console)
+    listaJogos.append(novoJogo)
+    return render_template('lista.html', titulo='Games',jogos=listaJogos)
+
+app.run(debug=True)
